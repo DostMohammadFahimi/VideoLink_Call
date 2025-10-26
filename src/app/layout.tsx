869d88f6759ad8ee20f09587c96802fe64d07f7d
Dost Mohammad Fahimi <dmfahimiiiii730@gmail.com>
2025-10-26 08:35:17 +0430

    implement the  client provider and Error tage and also  action.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
+import Navbar from "@/components/Navbar";
+import {
+  ClerkProvider
+} from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import ClientProvider from "./ClientProvider";
 import "./globals.css";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
-import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,22 +23,10 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          {/* <header className="flex h-16 items-center justify-end gap-4 p-4">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="h-10 cursor-pointer rounded-full bg-[#6c47ff] px-4 text-sm font-medium text-white sm:h-12 sm:px-5 sm:text-base">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header> */}
-
-          <Navbar />
-          <main className="mx-auto max-w-5xl px-3 py-6">{children}</main>
+          <ClientProvider>
+            <Navbar />
+            <main className="mx-auto max-w-5xl px-3 py-6">{children}</main>
+          </ClientProvider>
         </body>
       </html>
     </ClerkProvider>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
         <Link href="/">New Meeting</Link>
         <SignedIn>
           <div className="flex items-center gap-5">
-            <Link href="meetings">Meetings</Link>
+            <Link href="/meetings">Meetings</Link>
             <UserButton />
           </div>
         </SignedIn>
